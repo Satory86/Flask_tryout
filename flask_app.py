80fe1fca881a79aba4c844b0178ad9132bd33a1c
Zuzanna Satory <satory86@gmail.com>
2023-08-03 16:59:14 +0200

    basic Flask code update

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,6 @@ from flask import Flask, Markup
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-    return Markup("&lt;p&gt;Hello World!&lt;/p&gt;")
+    return Markup("&lt;p&gt;Hello docent Peter! Na veel problemen met running flask maak ik deze les na bijna 8 uur af!!&lt;/p&gt;")
 if __name__== '__main__':
     app.run(poort=5000, debug=True)
